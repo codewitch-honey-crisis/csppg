723d2b6c948568b5db1dc3b1d5f83a2a44f7774e
honey the codewitch <gaztoast@gmail.com>
2021-10-28 04:31:42 -0700

    Initial commit

---
+++ csppg/Preprocessor.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace csppg
+{
+    class Preprocessor
+    {
+		public static void Run(TextReader input, TextWriter output, IDictionary<string, object> args = null, string codeclass="Preprocessor", string codenamespace=null, TextReader codebehind = null, bool generatePreprocessor=false,bool @internal = false)
+        {
+			if (string.IsNullOrWhiteSpace(codeclass))
+				codeclass = "Preprocessor";
+			if (string.IsNullOrWhiteSpace(codenamespace))
+				codenamespace = null;
+			var frameworkPath = RuntimeEnvironment.GetRuntimeDirectory();
+			var cwd = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+			try
+			{
+				Directory.CreateDirectory(Path.Combine(cwd, "temp"));
+			}
+			catch
+            {
+
+            }
+			if (!generatePreprocessor)
+			{
+				cwd = Path.Combine(cwd, "temp");
+				foreach (var file in Directory.GetFiles(cwd))
+				{
+					try
+					{
+						File.Delete(file);
+					}
+					catch { }
+				}
+			}
+			var sb = new StringBuilder();
+			var sw = (!generatePreprocessor)?new StringWriter(sb):output;
+			sw.WriteLine("using System;");
+			sw.WriteLine("using System.IO;");
+			sw.WriteLine("using System.Text;");
+			sw.WriteLine("using System.Collections.Generic;");
+			if(codenamespace!=null)
+            {
+				sw.Write("namespace {0} ", codenamespace);
+				sw.WriteLine("{");
+            }
+			sw.Write("{0}{2} partial class {1} ",codenamespace!=null?"    ":"", codeclass,@internal && generatePreprocessor?"internal":"public");
+			sw.WriteLine("{");
+			if (codenamespace != null) sw.Write("    ");
+			sw.WriteLine("    public static void Run(TextWriter Response, IDictionary<string, object> Arguments) {");
+			int cur;
+			var more = true;
+			var cch = '\0'; 
+			while (more)
+			{
+				char ccch;
+				var text = _ReadUntilStartContext(input,out ccch);
+				if (cch == 0) cch = ccch; else if (ccch != cch && 0!=ccch) throw new InvalidOperationException("Invalid mixing and matching of context switches");
+				if (0 < text.Length)
+				{
+					var srl = new StringReader(text);
+					char[] buf = new char[1024];
+					var r = srl.Read(buf, 0, buf.Length);
+					while (0!=r)
+					{
+						if (codenamespace != null) sw.Write("    ");
+						sw.Write("        Response.Write(");
+						var s = _ToStringLiteral(buf,r);
+						sw.Write(s); 
+						sw.WriteLine(");");
+						r = srl.Read(buf, 0, buf.Length);
+					}
+				}
+				cur = input.Read();
+				if (-1 == cur)
+					more = false;
+				else if ('=' == cur)
+				{
+					if (codenamespace != null) sw.Write("    ");
+					sw.Write("        Response.Write(");
+					sw.Write(_ReadUntilEndContext(-1, input, cch));
+					sw.WriteLine(");");
+				}
+				else
+					sw.WriteLine(_ReadUntilEndContext(cur, input,cch));
+			}
+			if (codenamespace != null) sw.Write("    ");
+			sw.WriteLine("        Response.Flush();");
+			if (codenamespace != null) sw.Write("    ");
+			sw.WriteLine("    }");
+			if (codenamespace != null) sw.Write("    ");
+			sw.WriteLine("}");
+			if(codenamespace!=null) sw.WriteLine("}");
+			sw.Flush();
+			if (!generatePreprocessor)
+			{
+				var cscPath = Path.Combine(frameworkPath, "csc.exe");
+				var psi = new ProcessStartInfo(cscPath);
+				psi.UseShellExecute = false;
+				psi.CreateNoWindow = true;
+				psi.RedirectStandardError = true;
+				psi.RedirectStandardOutput = true;
+				var dllext = Path.GetExtension(typeof(int).Assembly.GetModules()[0].FullyQualifiedName);
+				var bfn = "Preprocess";
+				var ufn = bfn + dllext;
+				int ufi = 1;
+				while (File.Exists(Path.Combine(cwd, ufn)))
+					ufn = bfn + (++ufi).ToString() + dllext;
+				var csufn = bfn + ".cs";
+				ufi = 1;
+				while (File.Exists(Path.Combine(cwd, csufn)))
+					csufn = bfn + (++ufi).ToString() + ".cs";
+				var cbufn = "Codebehind.cs";
+				if (null != codebehind)
+				{
+					ufi = 1;
+					while (File.Exists(Path.Combine(cwd, cbufn)))
+						cbufn = "Codebehind" + (++ufi).ToString() + ".cs";
+					using (var fw = new StreamWriter(Path.Combine(cwd, cbufn), false, Encoding.UTF8))
+					{
+						char[] buf = new char[1024];
+						var r = codebehind.Read(buf, 0, buf.Length);
+						while (0 != r)
+						{
+							fw.Write(buf, 0, r);
+							r = codebehind.Read(buf, 0, buf.Length);
+						}
+						fw.Flush();
+					}
+				}
+				using (var fw = new StreamWriter(Path.Combine(cwd, csufn), false, Encoding.UTF8))
+				{
+					fw.BaseStream.SetLength(0);
+					fw.Write(sb.ToString());
+				}
+
+				psi.Arguments = "-target:library -out:\"" + Path.Combine(cwd, ufn).Replace("\"", "\"\"") + "\" \"" + Path.Combine(cwd, csufn).Replace("\"", "\"\"") + "\"";
+				if(codebehind!=null)
+                {
+					psi.Arguments += " \""+Path.Combine(cwd,cbufn).Replace("\"", "\"\"") + "\"";
+                }
+				var proc = Process.Start(psi);
+				proc.WaitForExit();
+				Assembly asm = null;
+				Exception ex = null;
+				if (File.Exists(Path.Combine(cwd, csufn)))
+				{
+					try
+					{
+						asm = Assembly.LoadFrom(Path.Combine(cwd, ufn));
+					}
+					catch (Exception e) { ex = new TargetInvocationException(proc.StandardOutput.ReadToEnd() + Environment.NewLine + proc.StandardError.ReadToEnd(), e); }
+
+				}
+				else
+				{
+					ex = new TargetInvocationException(proc.StandardOutput.ReadToEnd() + Environment.NewLine + proc.StandardError.ReadToEnd(), new Exception("Error compiling assembly."));
+				}
+				if (null != ex)
+				{
+					throw ex;
+				}
+				if (null == args) args = new Dictionary<string, object>();
+				if (null != asm)
+				{
+					var s = codeclass;
+					if (codenamespace != null) s = codenamespace + "." + codeclass;
+					var t = asm.GetType(s);
+					var m = t.GetMethod("Run");
+					if (null != m)
+					{
+						try
+						{
+							m.Invoke(null, new object[] { output, args });
+						}
+						catch (TargetInvocationException tex)
+						{
+							throw tex.InnerException;
+						}
+					}
+				}
+				File.Delete(csufn);
+				try
+				{
+					File.Delete(ufn);
+				}
+				catch { }
+			}
+			
+		}
+		static string _ReadUntilStartContext(TextReader input,out char ctxChar)
+		{
+			int cur = input.Read();
+			var sb = new StringBuilder();
+			while (true)
+			{
+				if ('<' == cur)
+				{
+					cur = input.Read();
+					if (-1 == cur)
+					{
+						sb.Append('<');
+						ctxChar = '\0';
+						return sb.ToString();
+					}
+					else if ('#' == cur || '%' == cur)
+					{
+						ctxChar = unchecked((char)cur);
+						return sb.ToString();
+					}
+					sb.Append('<');
+				}
+				else if (-1 == cur)
+				{
+					ctxChar = '\0';
+					return sb.ToString();
+				}
+				sb.Append(unchecked((char)cur));
+				cur = input.Read();
+			}
+		}
+		static string _ReadUntilEndContext(int firstChar, TextReader input,char ctxChar)
+		{
+			int cur;
+			cur = firstChar;
+			if (-1 == firstChar)
+				cur = input.Read();
+			var sb = new StringBuilder();
+			while (true)
+			{
+				if(ctxChar == cur)
+				{
+					cur = input.Read();
+					if (-1 == cur)
+					{
+						sb.Append(ctxChar);
+						return sb.ToString();
+					}
+					else if ('>' == cur)
+						return sb.ToString();
+					sb.Append(ctxChar);
+				}
+				else if (-1 == cur)
+					return sb.ToString();
+				sb.Append(unchecked((char)cur));
+				cur = input.Read();
+			}
+		}
+		static string _ToStringLiteral(IEnumerable<char> text, int r = -1)
+		{
+			var result = new StringBuilder();
+			result.Append("\"");
+			var i = 0;
+			foreach (var ch in text)
+			{
+				if (r > -1 && i == r)
+					break;
+				switch (ch)
+				{
+					case '\0':
+						result.Append("\0");
+						break;
+					case '\a':
+						result.Append("\a");
+						break;
+					case '\b':
+						result.Append("\\b");
+						break;
+					case '\f':
+						result.Append("\\f");
+						break;
+					case '\n':
+						result.Append("\\n");
+						break;
+					case '\r':
+						result.Append("\\r");
+						break;
+					case '\t':
+						result.Append("\\t");
+						break;
+					case '\v':
+						result.Append("\\v");
+						break;
+					case '\"':
+						result.Append("\\\"");
+						break;
+					case '\'':
+						result.Append("\\\'");
+						break;
+					case '\\':
+						result.Append("\\\\");
+						break;
+					default:
+						result.Append(ch);
+						break;
+				}
+				++i;
+			}
+			result.Append("\"");
+			return result.ToString();
+		}
+
+	}
+}
---
+++ csppg/Program.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csppg
+{
+    static class Program
+    {
+		static readonly string CodeBase = _GetCodeBase();
+		static readonly string Filename = Path.GetFileName(CodeBase);
+		static readonly string Name = _GetName();
+		static readonly Version Version = _GetVersion();
+		static readonly string Description = _GetDescription();
+		static int Main(string[] args) => Run(args, Console.In, Console.Out, Console.Error);
+		/// <summary>
+		/// Runs the process
+		/// </summary>
+		/// <param name="args">The arguments</param>
+		/// <param name="stdin">The input stream</param>
+		/// <param name="stdout">The output stream</param>
+		/// <param name="stderr">The error stream</param>
+		/// <returns>The error code</returns>
+		public static int Run(string[] args, TextReader stdin, TextWriter stdout, TextWriter stderr)
+		{
+			string inputfile=null;
+			bool @internal = false;
+			string outputfile=null;
+			string codeclass = null;
+			string codenamespace=null;
+			bool ifstale = false;
+			// our working variables
+			var result = 0;
+			TextReader input = null;
+			TextWriter output = null;
+			try
+			{
+				if (0 == args.Length)
+				{
+					result = -1;
+					_PrintUsage(Console.Error);
+					
+				}
+				else if (args[0].StartsWith("/"))
+				{
+					throw new ArgumentException("Missing input file.");
+				}
+				else
+				{
+					// process the command line args
+					inputfile = args[0];
+					for (var i = 1; i < args.Length; ++i)
+					{
+						switch (args[i].ToLowerInvariant())
+						{
+							case "/output":
+								if (args.Length - 1 == i) // check if we're at the end
+									throw new ArgumentException(string.Format("The parameter \"{0}\" is missing an argument", args[i].Substring(1)));
+								++i; // advance 
+								outputfile = args[i];
+								break;
+							case "/class":
+								if (args.Length - 1 == i) // check if we're at the end
+									throw new ArgumentException(string.Format("The parameter \"{0}\" is missing an argument", args[i].Substring(1)));
+								++i; // advance 
+								codeclass = args[i];
+								break;
+							case "/namespace":
+								if (args.Length - 1 == i) // check if we're at the end
+									throw new ArgumentException(string.Format("The parameter \"{0}\" is missing an argument", args[i].Substring(1)));
+								++i; // advance 
+								codenamespace = args[i];
+								break;
+							case "/internal":
+								@internal = true;
+								break;
+							case "/ifstale":
+								ifstale = true;
+								break;
+							default:
+								throw new ArgumentException(string.Format("Unknown switch {0}", args[i]));
+						}
+					}
+				}
+				if (string.IsNullOrWhiteSpace(inputfile))
+					throw new ArgumentException("inputfile");
+				var cwd = Environment.CurrentDirectory;
+				if (!ifstale || _IsStale(inputfile, outputfile))
+				{
+					if (null != outputfile)
+					{
+						Console.Error.WriteLine("{0} is building file: {1}", Name, outputfile);
+						cwd = Path.GetDirectoryName(outputfile);
+						output = new StreamWriter(outputfile);
+					}
+					else
+					{
+						Console.Error.WriteLine("{0} is building preprocessor.", Name);
+						output = Console.Out;
+					}
+					if (string.IsNullOrEmpty(codeclass))
+					{
+						// default we want it to be named after the code file
+						// otherwise we'll use inputfile
+						if (null != outputfile)
+							codeclass = Path.GetFileNameWithoutExtension(outputfile);
+						else
+							codeclass = Path.GetFileNameWithoutExtension(inputfile);
+					}
+					input = new StreamReader(inputfile);
+					Preprocessor.Run(input, output, null, codeclass, codenamespace, null, true, @internal);
+				} else
+                {
+					stderr.WriteLine("{0} skipped building of {1} because it was not stale.", Name, outputfile);
+                }
+
+
+			}
+			// we don't like to catch in debug mode
+#if !DEBUG
+			catch (Exception ex)
+			{
+				result = _ReportError(ex, stderr);
+			}
+#endif
+			finally
+			{
+				// close the input file if necessary
+				if (null != input)
+					input.Close();
+				// close the output file if necessary
+				if (null != outputfile && null != output)
+					output.Close();
+			}
+			
+			return result;
+		}
+		static void _PrintUsage(TextWriter w)
+		{
+			w.Write("Usage: " + Filename + " ");
+			w.WriteLine("<inputfile> [/output <outputfile>] [/class <codeclass>]");
+			w.WriteLine("   [/namespace <codenamespace>] [/internal] [/ifstale]");
+			w.WriteLine();
+
+			w.Write(Name);
+			w.Write(" ");
+			w.Write(Version.ToString());
+			if (!string.IsNullOrWhiteSpace(Description))
+			{
+				w.Write(" - ");
+				w.WriteLine(Description);
+			}
+			else
+			{
+				w.WriteLine("<No description>");
+			}
+			w.WriteLine();
+			w.WriteLine("   <inputfile>     The input template");
+			w.WriteLine("   <outputfile>    The preprocessor source file - defaults to STDOUT");
+			w.WriteLine("   <codeclass>     The name of the main class to generate - default derived from <outputfile>");
+			w.WriteLine("   <codenamespace> The namespace to generate the code under - defaults to none");
+			w.WriteLine("   <internal>      Mark the generated class as internal - defaults to public");
+			w.WriteLine("   <ifstale>       Only generate if the input is newer than the output");
+			w.WriteLine();
+		}
+		static bool _IsStale(string inputfile, string outputfile)
+		{
+			if (string.IsNullOrEmpty(outputfile) || string.IsNullOrEmpty(inputfile))
+				return true;
+			var result = true;
+			// File.Exists doesn't always work right
+			try
+			{
+				if (File.GetLastWriteTimeUtc(outputfile) >= File.GetLastWriteTimeUtc(inputfile))
+					result = false;
+			}
+			catch { }
+			return result;
+		}
+		static string _GetCodeBase()
+		{
+			try { return Assembly.GetExecutingAssembly().GetModules()[0].FullyQualifiedName; }
+			catch { return Path.Combine(Environment.CurrentDirectory, typeof(Program).Namespace+".exe"); }
+		}
+		static string _GetName()
+		{
+			try
+			{
+				foreach (var attr in Assembly.GetExecutingAssembly().CustomAttributes)
+				{
+					if (typeof(AssemblyTitleAttribute) == attr.AttributeType)
+					{
+						return attr.ConstructorArguments[0].Value as string;
+					}
+				}
+			}
+			catch { }
+			return Path.GetFileNameWithoutExtension(Filename);
+		}
+		static Version _GetVersion()
+		{
+			return Assembly.GetExecutingAssembly().GetName().Version;
+		}
+		static string _GetDescription()
+		{
+			string result = null;
+			foreach (Attribute ca in Assembly.GetExecutingAssembly().GetCustomAttributes())
+			{
+				var ada = ca as AssemblyDescriptionAttribute;
+				if (null != ada && !string.IsNullOrWhiteSpace(ada.Description))
+				{
+					result = ada.Description;
+					break;
+				}
+			}
+			return result;
+		}
+
+		// do our error handling here (release builds)
+		static int _ReportError(Exception ex, TextWriter stderr)
+		{
+			_PrintUsage(stderr);
+			stderr.WriteLine("Error: {0}", ex.Message);
+			return -1;
+		}
+	}
+}
